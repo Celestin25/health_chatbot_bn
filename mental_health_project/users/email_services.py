8c9e04b7d5e81c8d5a4bea0368f64bebdabc040c
Celestin25 <99292347+Celestin25@users.noreply.github.com>
2024-11-01 22:14:55 +0200

    Update email_services.py

--- mental_health_project/users/email_services.py
+++ mental_health_project/users/email_services.py
@@ -20,26 +20,14 @@ class EmailThread(threading.Thread):
 
 generate_email_message = lambda user_first_name: f"""
 Hi {user_first_name},
+Welcome to e-health
 
-Welcome aboard to ALU FabLab Booking System! We're thrilled to have you as a part of our community of innovators and creators.
-
-Your registration is now complete, and we've sent a verification email to the address you provided. To ensure the security of your account and access all the functionalities of our platform, please take a moment to verify your email address by clicking the verification link in the email.
-
-We're dedicated to providing you with a seamless experience in reserving equipment for your projects. Whether you're delving into 3D printing, laser cutting, or any other fabrication endeavor, our system is designed to make the process effortless and efficient.
-
-Should you encounter any questions or need assistance, our support team is here to help. We're committed to ensuring your experience with our Fab Lab booking system is nothing short of exceptional.
-
-Once again, welcome! We look forward to seeing you thrive in our vibrant community.
-
-Best regards,
-
-Your friends at FabLab.
 """
 
 
 class EmailService:
     def send_welcome_email(self, user):
-        subject = "Welcome to ALU FabLab Booking System"
+        subject = ""
         message = generate_email_message(user.first_name)
         welcome_email = EmailMessage(
             subject,
@@ -51,7 +39,7 @@ class EmailService:
 
     def send_account_verification_email(self, request, user):
         current_site = get_current_site(request)
-        email_confirmation_subject = "Verify Your Email - ALU FabLab"
+        email_confirmation_subject = ""
         email_confirmation_message = render_to_string('email_confirmation.html', {
             'name': user.first_name,
             'domain': current_site.domain,
